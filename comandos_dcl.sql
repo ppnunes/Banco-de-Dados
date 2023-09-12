4bd7a6f2a25b89beddb5769582a7008238ce1cde
Priscila Nunes <103075521+ppnunes@users.noreply.github.com>
2023-09-12 20:23:16 -0300

    adiciona conteúdo da aula de comandos dcl de 12.09

---
+++ comandos_dcl.sql
@@ -0,0 +1,33 @@
+use bd_rh_terca_noite;
+
+select * from tb_funcionario;
+
+-- mostra o banco de dados:
+show databases;
+
+-- mostra usuários:
+select user from mysql.user;
+
+-- mostra o usuario e onde ele foi criado:
+select user, host from mysql.user;
+
+-- para criar um usuario:
+create user alunotn@localhost IDENTIFIED BY 'senha';
+
+-- Listar os usuários:
+select user from mysql.user;
+
+-- * DCL - Privilégios *:
+-- INSERT		Inserir dados em uma tabela
+-- UPDATE		Atualizar dados em uma tabela
+-- DELETE		Excluir dados de uma tabela 
+-- EXECUTE		Executar funções ou procedimentos armazenados
+-- SELECT		Efetuar consultas em uma tabela
+-- GRANT 		Dar acesso
+
+-- Dar acesso ao banco bd_rh_terca_noite, somente à tabela funcionario, para leitura e alteração:
+GRANT select, update
+ON bd_rh_terca_noite.tb_funcionario
+TO alunotn@localhost;
+-- para dar acesso a todo o banco, colocar ON bd_rh_terca_noite *
+
